f00fcae370f56ed07a5c068eac253e39e91538f7
AshenSankalpa <ashensankalpa6@gmail.com>
2025-04-27 13:33:26 +0530

    create function and arithmetic operation

---
+++ prog.js
@@ -0,0 +1,13 @@
+let x = 10
+let y = 15
+
+console.log("Addition: " + (x + y))
+console.log("Subtraction: " + (y - x))
+console.log("Muliplication: " + (x * y))
+console.log("Division: " + (y/x))
+
+let a = 10;
+function test(a) {
+     console.log(a);
+}
+test(20);
